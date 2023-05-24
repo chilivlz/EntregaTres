5b79085901a519253b293a79525689624636b4a7
chilivlz <johanachilito2@gmail.com>
2023-05-24 00:06:56 -0500

    "se limipia el codigo"

--- PimeraPre-Entrega/src/routes/carts.router.js
+++ PimeraPre-Entrega/src/routes/carts.router.js
@@ -10,7 +10,6 @@ cartsRouter.use(express.urlencoded({ extended: true }));
 
 cartsRouter.post("/", async (req, res) => {
   const userCart = await cartManager.createCart();
-
   res.status(201).send({ status: "success", data: userCart });
 });
 
@@ -18,7 +17,7 @@ cartsRouter.get("/:cid", async (req, res) => {
   const cartId = req.params.cid
   const cartFound = await cartManager.getCartById(cartId)
   res.status(200).send({status: "success", msg: "carrito encontrado", data: cartFound,})
-  res.send(cartFound)
+  
 });
 
 cartsRouter.post("/:cid/product/:pid", async (req, res) => {
@@ -26,5 +25,5 @@ cartsRouter.post("/:cid/product/:pid", async (req, res) => {
   const productId = req.params.pid
   cartManager.addProductToCart(cartId,productId)
   res.status(200).send({status:"sucess",msm:"se agrego product", data:cartManager })
-  res.send("se agrego el producto")
+ 
 });
